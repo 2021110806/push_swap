ec14b7527eb37b1c303164a404e5e52ee0772ebb
2021110806 <2021110806@dgu.ac.kr>
2023-07-25 23:32:26 +0900

    style: 불필요한 내용 주석처리

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: minjeon2 <qwer10897@naver.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 00:59:56 by minjeon2          #+#    #+#             */
-/*   Updated: 2023/07/25 20:45:28 by minjeon2         ###   ########.fr       */
+/*   Updated: 2023/07/25 22:58:49 by minjeon2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,7 @@ int rotate_stack_a(t_linked_list *stack_a, t_moving_counter *moving_counter);
 int	is_sorted(t_linked_list *list);
 int count_command_in_stack_a(int target, t_linked_list *stack_a, t_moving_counter *moving_counter);
 int count_command_in_stack_b(int idx, t_linked_list *stack_b, t_moving_counter *moving_counter);
+int move_min_value_to_top(t_linked_list *stack_a);
 t_moving_counter	*find_minimum_greedy(t_linked_list *stack_a, t_linked_list *stack_b);
 int	rotate(t_linked_list *stack_a, t_linked_list *stack_b, t_moving_counter *moving_counter);
 int	reverse_rotate(t_linked_list *stack_a, t_linked_list *stack_b, t_moving_counter *moving_counter);
--- utils.c
+++ utils.c
@@ -1,6 +1,42 @@
+#include "push_swap.h"
+
 int	abs_(int inp)
 {
 	if (inp < 0)
 		return (-1 * inp);
 	return (inp);
+}
+
+int move_min_value_to_top(t_linked_list *stack_a)
+{
+	int 	min_value;
+	int		cnt;
+	t_node *node;
+
+	cnt = 0;
+	min_value = min(stack_a);
+	node = stack_a -> head -> next;
+	while (node -> item != min_value)
+	{
+		cnt++;
+		node = node -> next;
+	}
+	if (cnt < stack_a -> size / 2)
+	{
+		while (cnt)
+		{
+			ra(stack_a);
+			cnt--;
+		}
+	}
+	else
+	{
+		cnt = stack_a -> size - cnt;
+		while (cnt)
+		{
+			rra(stack_a);
+			cnt--;
+		}
+	}
+	return (1);
 }
